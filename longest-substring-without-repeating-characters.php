a0a87aa1d76ff9f275b93dc4bb3125d85a910544
Ravi Tiwari <ravi.tiwari@cgparivar.com>
2021-09-21 20:06:43 +0530

    Completed sum of two linked list.

---
+++ longest-substring-without-repeating-characters.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class Solution
+ * @link https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+class Solution
+{
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLongestSubstring($s)
+    {
+        $stringInArray = [];
+        for ($j = 0; $j < strlen($s); $j++) {
+
+        }
+    }
+}
